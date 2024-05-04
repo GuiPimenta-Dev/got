1b01ba5f795ca49429977bcf9b01232ca7b538cb
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-04 16:13:12 -0300

    fix(setup.py): update package version to 0.0.3 in setup configurations

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="got",
-    version="0.0.1",
+    version="0.0.3",
     packages=find_packages(),
     license="MIT",
     install_requires=[
