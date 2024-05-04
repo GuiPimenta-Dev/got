3f9622240b96f9f5d06344a8021d4e44a97028cd
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-04 17:13:22 -0300

    build(setup): update package version to 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="got-cli",
-    version="0.0.3",
+    version="0.0.7",
     packages=find_packages(),
     license="MIT",
     install_requires=[
