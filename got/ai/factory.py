43bf797a63d4bca4f5f9fd558027f7c681471fba
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-04 15:19:03 -0300

    refactor(scope): improve code readability

--- got/ai/factory.py
+++ got/ai/factory.py
@@ -1,9 +1,22 @@
-
+import os
+from dotenv import load_dotenv
 
 from got.ai import AI
 from got.ai.chatgpt import ChatGPT
+from got.ai.groq import GroqCloud
+
+load_dotenv()
+LLM_MODEL = os.environ.get("LLM_MODEL")
+
+OPENAI_MODELS = ["gpt-3.5-turbo"]
+GROQ_MODELS = ["gemma-7b-it", "llama2-70b-4096", "llama3-70b-8192", "llama3-8b-8192", "mixtral-8x7b-32768"]
 
 
 class AIFactory:
-    def create_ai(self) -> AI:
-        return ChatGPT("gpt-3.5-turbo")
+
+    def create_ai(self, model) -> AI:
+
+        if model in OPENAI_MODELS:
+            return ChatGPT(model)
+        elif model in GROQ_MODELS:
+            return GroqCloud(model)
