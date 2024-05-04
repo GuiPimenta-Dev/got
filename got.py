ecd2a5e54a1f82b858b71574b7eb801365a385b9
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-04 14:00:02 -0300

    style(got): improve formatting in got.py

--- got.py
+++ got.py
@@ -1,5 +1,5 @@
 import click
-from commit import Commit
+from commit import CommitHandler
 from InquirerPy import inquirer
 from InquirerPy.separator import Separator
 from prompt_toolkit import prompt
@@ -7,72 +7,69 @@ from prompt_toolkit.history import InMemoryHistory
 
 from git import Git
 from printer import Printer
+
 printer = Printer()
 git = Git()
-commit_handler = Commit()
+commit_handler = CommitHandler()
 
 commits = commit_handler.get_commit_message_suggestion()
 
 for commit in commits:
-  lines = max(len(string) for string in commit["messages"])
-  choices = [*commit["messages"],Separator(lines * "-"),"Retry", "Skip", "Abort"]
+    lines = max(len(string) for string in commit["messages"])
+    choices = [*commit["messages"], Separator(lines * "-"), "Retry", "Skip", "Abort"]
 
-  click.echo()
-  commit_message = inquirer.select(
-            message=f"Select a commit message for the following files: {commit['files']}",
-            choices=choices,
+    click.echo()
+    commit_message = inquirer.select(
+        message=f"Select a commit message for the following files: {commit['files']}",
+        choices=choices,
+    ).execute()
 
-        ).execute()
-  
-  if commit_message == "Retry":
-    while True:
-      messages = commit_handler.retry_commit_message_suggestion(commit["changes"])
-      lines = max(len(string) for string in messages)
-      choices = [*messages,Separator(lines * "-"),"Retry", "Skip", "Abort"]
+    if commit_message == "Retry":
+        while True:
+            messages = commit_handler.retry_commit_message_suggestion(commit["changes"])
+            lines = max(len(string) for string in messages)
+            choices = [*messages, Separator(lines * "-"), "Retry", "Skip", "Abort"]
 
-      click.echo()
-      commit_message = inquirer.select(
+            click.echo()
+            commit_message = inquirer.select(
                 message=f"Select a commit message for the following files: {commit['files']}",
                 choices=choices,
-
             ).execute()
 
-      if commit_message != "Retry":
-        break
-      
-  if commit_message == "Skip":
-    continue
-  
-  if commit_message == "Abort":
-    break
-  
-  change_commit_message = inquirer.select(
-            message=f"Would you like to edit the selected message?",
-            choices=["No", "Yes"],
-        ).execute()
-  
-  
-  if change_commit_message == "Yes":
-    history = InMemoryHistory()
-    history.append_string(commit_message)
-    click.echo()
-    commit_message = prompt("", default=commit_message, history=history)
+            if commit_message != "Retry":
+                break
 
-    
-  git.add_commit({"files_to_commit": commit["files"], "message": commit_message})
+    if commit_message == "Skip":
+        continue
+
+    if commit_message == "Abort":
+        break
+
+    change_commit_message = inquirer.select(
+        message=f"Would you like to edit the selected message?",
+        choices=["No", "Yes"],
+    ).execute()
+
+    if change_commit_message == "Yes":
+        history = InMemoryHistory()
+        history.append_string(commit_message)
+        click.echo()
+        commit_message = prompt("", default=commit_message, history=history)
+
+    git.add_commit({"files_to_commit": commit["files"], "message": commit_message})
 
 printer.print("Commits to be made:", "blue", 1)
 for commit in git.commits:
-  printer.print("------------------------ + ------------------------", "gray", 1)
+    printer.print("------------------------ + ------------------------", "gray", 1)
 
-  printer.print("Files:", "white",1, 1 )
-  for file in commit["files_to_commit"]:
-    printer.print(f"  {file}", "green")
-  
-  printer.print("Message:", "white", 1 , 1)
-  printer.print(f"  {commit['message']}", "yellow")
+    printer.print("Files:", "white", 1, 1)
+    for file in commit["files_to_commit"]:
+        printer.print(f"  {file}", "green")
+
+    printer.print("Message:", "white", 1, 1)
+    printer.print(f"  {commit['message']}", "yellow")
 
 click.echo()
 
 if click.confirm("Would you like to proceed?", default="Y", abort=True):
-  git.commit()
+    git.commit()
