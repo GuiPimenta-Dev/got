94bb82b4f3c33e321d2c03ae41de92e1173b5028
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-05 08:36:34 -0300

    bump(version): update got-cli version from 0.0.13 to 0.0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="got-cli",
-    version="0.0.13",
+    version="0.0.14",
     packages=find_packages(),
     license="MIT",
     install_requires=[
