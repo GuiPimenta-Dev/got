4a1198377e3271c4596e34716eecc763aea2e314
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-05 14:08:44 -0300

    feat(setup): bump version to 0.0.20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 #
 setup(
     name="got-cli",
-    version="0.0.19",
+    version="0.0.20",
     packages=find_packages(),
     license="MIT",
     install_requires=[
